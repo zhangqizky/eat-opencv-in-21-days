9b386b13e10f237c5bebfd7456c9d8bfb393cda2
buaazhangqi <2445657635@qq.com>
2020-09-15 23:29:19 +0800

    Create documentations.cpp

---
+++ Introduction/documentations.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+/**
+ * @function main
+ * @brief Main function这儿写关于main函数的注释和详细说明
+ */
+int main( void )
+{
+    //! [hello_world]
+    std::cout << "Hello World!";
+    //! [hello_world]
+
+    return 0;
+}
